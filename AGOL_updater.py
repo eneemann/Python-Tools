8ee7875731d55e8f735cf8e6d930cf0752099e04
Erik Neemann <eneemann@utah.gov>
2020-05-01 13:19:38 -0600

    updated field mappings to by day table for new fields

--- AGOL_updater.py
+++ AGOL_updater.py
@@ -17,7 +17,7 @@ import numpy as np
 import datetime as dt
 import pytz
 
-# print(dt.datetime.now())
+print(dt.datetime.now())
 
 
 # Start timer and print start time in UTC
@@ -31,11 +31,10 @@ print(portal_url)
 
 user = getpass.getpass(prompt='    Enter arcgis.com username:\n')
 pw = getpass.getpass(prompt='    Enter arcgis.com password:\n')
+# gis = GIS(portal_url, 'Erik.Neemann@UtahAGRC', pw)
 arcpy.SignInToPortal(portal_url, user, pw)
 del pw
 
-# old TEST layer
-# counts_service = r'https://services1.arcgis.com/99lidPhWCzftIe9K/ArcGIS/rest/services/EMN_Cases_by_LHD_TEST/FeatureServer/0'
 # TEST layer
 counts_service = r'https://services1.arcgis.com/99lidPhWCzftIe9K/ArcGIS/rest/services/EMN_Cases_by_LHD_TEST_v3/FeatureServer/0'
 # TEST table
@@ -43,11 +42,11 @@ counts_by_day = r'https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/serv
 
 
 work_dir = r'C:/Users/eneemann/Desktop/Neemann/COVID19'
-updates = pd.read_csv(os.path.join(work_dir, 'COVID_Case_Counts_20200428.csv'))
+updates = pd.read_csv(os.path.join(work_dir, 'COVID_Case_Counts_latest.csv'))
 updates.sort_values('Jurisdiction', inplace=True)
 
 
-# UPDATE CASE COUNTS LAYER FROM MOST RECENT CSV
+# UPDATE LATEST CASE COUNTS LAYER FROM MOST RECENT CSV
 count = 0
 #             0                     1                      2                    3   
 fields = ['DISTNAME', 'COVID_Cases_Utah_Resident', 'COVID_Cases_Total', 'Hospitalizations',
@@ -63,8 +62,6 @@ with arcpy.da.UpdateCursor(counts_service, fields) as ucursor:
         row[3] = temp_df.iloc[0]['Hospitalizations']
         row[4] = dt.datetime.now()
         row[6] = (row[2]/row[5])*100000.
-        # if row[0] == '0' or row[0] == None:    
-        #     row[1] = 'B'
         count += 1
         ucursor.updateRow(row)
 print(f'Total count of COVID Case Count updates is: {count}')
@@ -92,15 +89,21 @@ fm_dict = {'DISTNAME': 'DISTNAME',
             'Date_Updated': 'Day',
             'Hospitalizations': 'Hospitalizations',
             'Population': 'Population',
-            'Cases_per_100k': 'Cases_per_100k'}
+            'Cases_per_100k': 'Cases_per_100k',
+            'COVID_Cases_Daily_Increase': 'COVID_Cases_Daily_Increase',
+            'COVID_Total_Recoveries': 'COVID_Total_Recoveries',
+            'COVID_New_Daily_Recoveries': 'COVID_New_Daily_Recoveries',
+            'COVID_Total_Deaths': 'COVID_Total_Deaths',
+            'COVID_Deaths_Daily_Increase': 'COVID_Deaths_Daily_Increase'
+            }
 
 for key in fm_dict:
-    fm_name = arcpy.FieldMap()
-    fm_name.addInputField(counts_service, key)
-    output = fm_name.outputField
+    fm = arcpy.FieldMap()
+    fm.addInputField(counts_service, key)
+    output = fm.outputField
     output.name = fm_dict[key]
-    fm_name.outputField = output
-    fms.addFieldMap(fm_name)
+    fm.outputField = output
+    fms.addFieldMap(fm)
 
 counts_fields = [f.name for f in arcpy.ListFields(counts_service)]
 by_day_fields = [f.name for f in arcpy.ListFields(counts_by_day)]
@@ -119,13 +122,11 @@ if arcpy.Exists('in_memory\\temp_table'):
     print("Deleting 'in_memory\\temp_table' ...")
     arcpy.Delete_management('in_memory\\temp_table')
 
-# Convert countsby_day into pandas dataframe
+# Convert counts_by_day into pandas dataframe
 arcpy.conversion.TableToTable(counts_by_day, 'in_memory', 'temp_table')
-# day_arr = arcpy.da.TableToNumPyArray(counts_by_day, keep_fields)
-temp_fields = [f.name for f in arcpy.ListFields('in_memory\\temp_table')]
-
+# temp_fields = [f.name for f in arcpy.ListFields('in_memory\\temp_table')]
 day_arr = arcpy.da.TableToNumPyArray('in_memory\\temp_table', keep_fields)
-day_df = pd.DataFrame(data = day_arr)
+day_df = pd.DataFrame(data=day_arr)
 
 # Convert string entries of 'None' to zeros ('0')
 mask = day_df.applymap(lambda x: x == 'None')
@@ -133,45 +134,54 @@ cols = day_df.columns[(mask).any()]
 for col in day_df[cols]:
     day_df.loc[mask[col], col] = '0'
 
-# values = {'COVID_Cases_Daily_Increase': '0',
-#           'COVID_Total_Recoveries': '0',
-#           'COVID_New_Daily_Recoveries': '0',
-#           'COVID_Total_Deaths': '0',
-#           'COVID_Deaths_Daily_Increase': '0'}
-# day_df.fillna(value=values, inplace=True)
-
 day_df.head()
 day_df.sort_values('Day', inplace=True, ascending=True)
 day_df.head().to_string()
 
-# Create CSV for test data, update manually, read back in, overwrite working data
-day_df.to_csv(os.path.join(work_dir, 'by_day_testing.csv'))
+# Load test data
 test_df = pd.read_csv(os.path.join(work_dir, 'by_day_testing.csv'))
 
-# CHANGE code below back to reading day_df to test real data
+# Rename variables below to day_df after done testing
 
-# Split table into individual dfs by healt district
+# Split table into individual dfs by health district
 hd_list = test_df.DISTNAME.unique()
 hd_dict = {}
 for item in hd_list:
     temp = test_df[test_df.DISTNAME == item]
     hd_dict[item] = temp.reset_index()
 
-
 for key in hd_dict:
     for i in np.arange(1, hd_dict[key].shape[0]):
+        # Calculate daily case increase
         hd_dict[key].at[i, 'COVID_Cases_Daily_Increase'] = hd_dict[key].iloc[i]['COVID_Cases_Utah_Resident'] - hd_dict[key].iloc[i-1]['COVID_Cases_Utah_Resident']
+        # Calculate daily death increase
         hd_dict[key].at[i, 'COVID_Deaths_Daily_Increase'] = int(hd_dict[key].iloc[i]['COVID_Total_Deaths']) - int(hd_dict[key].iloc[i-1]['COVID_Total_Deaths'])
-        hd_dict[key].at[i, 'COVID_Total_Recoveries'] = int(hd_dict[key].iloc[i]['COVID_Cases_Utah_Resident']) - int(hd_dict[key].iloc[i]['COVID_Total_Deaths']) - ( int(hd_dict['Salt Lake County'].iloc[50]['COVID_Cases_Utah_Resident']) - int(hd_dict['Salt Lake County'].iloc[50-21]['COVID_Cases_Utah_Resident']) )
+        # Calculate daily total recoveries
+        if i > 20:
+            hd_dict[key].at[i, 'COVID_Total_Recoveries'] = int(hd_dict[key].iloc[i]['COVID_Cases_Utah_Resident']) - int(hd_dict[key].iloc[i]['COVID_Total_Deaths']) - ( int(hd_dict[key].iloc[i]['COVID_Cases_Utah_Resident']) - int(hd_dict[key].iloc[i-21]['COVID_Cases_Utah_Resident']) )
+        # Calculate daily recovery increase
+        hd_dict[key].at[i, 'COVID_New_Daily_Recoveries'] = int(hd_dict[key].iloc[i]['COVID_Total_Recoveries']) - int(hd_dict[key].iloc[i-1]['COVID_Total_Recoveries'])
 
 
-
-hd_dict['Salt Lake County'].at[50, 'COVID_Total_Recoveries'] = int(hd_dict['Salt Lake County'].iloc[50]['COVID_Cases_Utah_Resident']) - int(hd_dict['Salt Lake County'].iloc[50]['COVID_Total_Deaths']) - ( int(hd_dict['Salt Lake County'].iloc[50]['COVID_Cases_Utah_Resident']) - int(hd_dict['Salt Lake County'].iloc[50-21]['COVID_Cases_Utah_Resident']) )
-                                                                                                                    
-                                                                                                                    
-# total recovered = total cases - total deaths - cases within the last 21 days
-
-# daily recovered = total cases last 21 days - total deaths last 21 days
+# UPDATE COUNTS BY DAY TABLE WITH NEW NUMBERS
+table_count = 0
+#                   0                    1                           2   
+table_fields = ['DISTNAME', 'COVID_Cases_Daily_Increase', 'COVID_Total_Recoveries',
+          #            3                         4                          5
+          'COVID_New_Daily_Recoveries', 'COVID_Total_Deaths', 'COVID_Deaths_Daily_Increase']
+with arcpy.da.UpdateCursor(counts_by_day, table_fields) as ucursor:
+    print("Looping through rows to make updates ...")
+    for row in ucursor:
+        temp_df = updates.loc[updates['Jurisdiction'] == row[0]]
+        print(temp_df.head())
+        row[1] = temp_df.iloc[0]['Cases']
+        row[2] = row[1]
+        row[3] = temp_df.iloc[0]['Hospitalizations']
+        row[4] = dt.datetime.now()
+        row[6] = (row[2]/row[5])*100000.
+        table_count += 1
+        ucursor.updateRow(row)
+print(f'Total count of COVID Case Count updates is: {table_count}')      
     
 # COPY DAILY AND CUMULATIVE NUMBERS BACK TO MOST RECENT CASE COUNTS LAYER
 
@@ -186,24 +196,19 @@ print("Time elapsed: {:.2f}s".format(time.time() - start_time))
 
 
 
-# TESTING 
+# TESTING
+
 # 4/25 estimate for recovered
-# total recovered = total cases - total deaths - cases older than 21 days
 # total recovered = total cases - total deaths - cases within the last 21 days
 # total recovered = total cases - total deaths - (total cases today - total cases 22 days ago)
-
-# First attempt (older than 21 days)
-# Using current 4/25 total cases, current 4/25 total deaths, and current 4/4 total cases
+# First attempt (older than 21 days) - using total cases on 4/4
 r425 = 4140 - 45 - (4140 - 1592)
 print(r425)
 
-# Second attempt (older than 21 days)
-# Using current 4/25 total cases, old 4/25 total deaths, old 4/25 total cases, and current 4/4 total cases
-r425_v2 = 4140 - 43 - (4123 - 1592)     # gives web stie result (1566)
+# Second attempt (older than 21 days) - using total cases on 4/3
+r425_v2 = 4140 - 43 - (4123 - 1592)
 print(r425_v2)
 
 # What website is showing
 # 4140 cumulative
 # 1566 recovered
-# 2529 active
-# 45 dead
