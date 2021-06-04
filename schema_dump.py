8d4acf66758f81f184ffb303dac69b3fb6d61cbc
Erik Neemann <eneemann@utah.gov>
2021-06-04 14:19:11 -0600

    parameter changes to run code

--- schema_dump.py
+++ schema_dump.py
@@ -16,14 +16,14 @@ readable_start = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 print("The script start time is {}".format(readable_start))
 today = time.strftime("%Y%m%d")
 
-# Read in PLSS points from SGID
-geo_db = r"C:\E911\TOC\TOC_Geovalidation_WGS84.gdb"
-streets = os.path.join(geo_db, "Streets")
+# Point to desired geodatabases and feature classes
+geo_db = r"C:\E911\Layton\LaytonGeoValidation_updates.gdb"
+streets = os.path.join(geo_db, "LaytonStreets")
 streets_map = os.path.join(geo_db, "Streets_Map")
 
 
-classic_db = r'C:\E911\TOC\TOC_Spillman_WGS_84.gdb'
-classic_streets = os.path.join(classic_db, 'TOC_Streets')
+classic_db = r'C:\E911\Layton\QuickestRoute.gdb\QuickestRouteWGS_September13'
+QR_streets = os.path.join(classic_db, 'LaytonStreetsSeptember13')
 classic_streets_cad = os.path.join(classic_db, 'TOC_Streets_CAD')
 
 streets_fields = arcpy.ListFields(streets)
@@ -36,9 +36,9 @@ print('Streets_Map Field Info:')
 for f in streets_map_fields:
     print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
     
-classic_streets_fields = arcpy.ListFields(classic_streets)
-print('Classic Streets Field Info:')
-for f in classic_streets_fields:
+QR_streets_fields = arcpy.ListFields(QR_streets)
+print('QR streets Field Info:')
+for f in QR_streets_fields:
     print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
     
 classic_streets_cad_fields = arcpy.ListFields(classic_streets_cad)
