8b41e806832de567a711d90691b9df71c0d1908d
Erik Neemann <eneemann@utah.gov>
2021-12-20 15:00:45 -0700

    refactor: NG911 FCs

--- schema_dump.py
+++ schema_dump.py
@@ -18,30 +18,50 @@ today = time.strftime("%Y%m%d")
 
 # Point to desired geodatabases and feature classes
 geo_db = r"C:\E911\Layton\LaytonGeoValidation_updates.gdb"
-streets = os.path.join(geo_db, "LaytonStreets")
-streets_map = os.path.join(geo_db, "Streets_Map")
+county_db = r"C:\E911\Layton\DavisCoDispatchData_working.gdb"
+
+layton = os.path.join(geo_db, "PointsOfInterest")
+county = os.path.join(county_db, "CommonPlacePoints")
 
 
-classic_db = r'C:\E911\Layton\QuickestRoute.gdb\QuickestRouteWGS_September13'
-QR_streets = os.path.join(classic_db, 'LaytonStreetsSeptember13')
-classic_streets_cad = os.path.join(classic_db, 'TOC_Streets_CAD')
+#classic_db = r'C:\E911\Layton\QuickestRoute.gdb\QuickestRouteWGS_September13'
+#QR_streets = os.path.join(classic_db, 'LaytonStreetsSeptember13')
+#classic_streets_cad = os.path.join(classic_db, 'TOC_Streets_CAD')
 
-streets_fields = arcpy.ListFields(streets)
-print('Streets Field Info:')
-for f in streets_fields:
+#SGID = r"C:\Users\eneemann\AppData\Roaming\ESRI\ArcGISPro\Favorites\internal@SGID@internal.agrc.utah.gov.sde"
+#sgid = os.path.join(SGID, 'SOCIETY.PSAPBoundaries')
+#
+##NG911 = r"\\itwfpcap2\AGRC\agrc\data\ng911\SpatialStation_live_data\UtahNG911GIS.gdb"
+#NG911 = r"C:\Users\eneemann\Desktop\Neemann\NG911\NG911_project\NG911_boundary_work_testing.gdb"
+#ng911 = os.path.join(NG911, 'NG911_psap_bound_final_sgid_20210616')
+
+layton_fields = arcpy.ListFields(layton)
+print('Layton Field Info:')
+for f in layton_fields:
     print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
     
-streets_map_fields = arcpy.ListFields(streets_map)
-print('Streets_Map Field Info:')
-for f in streets_map_fields:
+county_fields = arcpy.ListFields(county)
+print('County Field Info:')
+for f in county_fields:
     print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
     
-QR_streets_fields = arcpy.ListFields(QR_streets)
-print('QR streets Field Info:')
-for f in QR_streets_fields:
-    print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
+#QR_streets_fields = arcpy.ListFields(QR_streets)
+#print('QR streets Field Info:')
+#for f in QR_streets_fields:
+#    print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
+#    
+#classic_streets_cad_fields = arcpy.ListFields(classic_streets_cad)
+#print('Classic Streets_CAD Field Info:')
+#for f in classic_streets_cad_fields:
+#    print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
     
-classic_streets_cad_fields = arcpy.ListFields(classic_streets_cad)
-print('Classic Streets_CAD Field Info:')
-for f in classic_streets_cad_fields:
-    print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
+    
+#sgid_fields = arcpy.ListFields(sgid)
+#print('SGID Field Info:')
+#for f in sgid_fields:
+#    print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
+#    
+#ng911_fields = arcpy.ListFields(ng911)
+#print('NG911 Field Info:')
+#for f in ng911_fields:
+#    print(f'Field Name: {f.name}     Field Type: {f.type}     Field Length: {f.length}')
