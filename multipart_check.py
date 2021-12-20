3f4b6caf5fd2c3cd6ad378c16adc69ffc3fc4fd0
Erik Neemann <eneemann@utah.gov>
2021-12-20 16:40:01 -0700

    intial version of code

---
+++ multipart_check.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 20 15:11:45 2021
+@author: eneemann
+
+Script to identify and list multipart features
+"""
+
+import arcpy
+import os
+import time
+
+# Start timer and print start time in UTC
+start_time = time.time()
+readable_start = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+print("The script start time is {}".format(readable_start))
+
+######################
+#  Set up variables  #
+######################
+
+today = time.strftime("%Y%m%d")
+db = r"L:\agrc\FromTheCounties\Emery_Co\Emery_precincts_2022.gdb"
+FC = os.path.join(db, r'Vista\Emery_vista_20211216')
+arcpy.env.workspace = db
+
+
+def multipart_check(fc):
+    multi_count = 0
+    multi_oids = []
+    fields = ['OID@', 'SHAPE@']
+    with arcpy.da.SearchCursor(fc, fields) as search_cursor:
+        print("Looping through rows in FC to check for multipart features ...")
+        for row in search_cursor:
+            shape_obj = row[1]
+            oid = row[0]
+            # print(shape_obj)
+            if shape_obj.partCount > 1:
+                multi_oids.append(oid)
+                multi_count += 1
+                print(f'part count of OID {oid}: {shape_obj.partCount}')
+                
+    print(f'Total count of multipart features: {multi_count}')
+    print('Multipart OIDs:')
+    for o in multi_oids:
+        print(o)
+            
+
+##########################
+#  Call Functions Below  #
+##########################
+
+multipart_check(FC)
+
+print("Script shutting down ...")
+# Stop timer and print end time in UTC
+readable_end = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+print("The script end time is {}".format(readable_end))
+print("Time elapsed: {:.2f}s".format(time.time() - start_time))
+    
